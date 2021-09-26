c714f61c1f3e500f8dee49eae6c14ac3d8ee7ece
Javier Alejandro Oyarzo Carcamo <javi_14_228@hotmail.com>
2021-09-26 17:19:27 -0300

    Add Models OK

--- models/mensajes.model.js
+++ models/mensajes.model.js
@@ -19,4 +19,52 @@ Mensaje.findAll = result => {
   });
 };
 
+Mensaje.create = function (newMessage, result) {
+  dbConn.query('INSERT INTO mensaje set ?', newMessage, function (err, res) {
+    if (err) {
+      console.log('error: ', err);
+      result(err, null);
+    }
+    else {
+      console.log(res.insertId);
+      result(null, res.insertId);
+    }
+  });
+};
+
+Mensaje.findById = function (id, result) {
+  dbConn.query('SELECT * FROM mensaje WHERE id_mensaje = ?', id, function (err, res) {
+    if (err) {
+      console.log('error: ', err);
+      result(err, null);
+    }
+    else {
+      result(null, res);
+    };
+  });
+};
+
+Mensaje.update = function(id, mensaje, result){
+  dbConn.query('UPDATE mensaje SET texto_mensaje=?, fecha_mensaje=?, id_usuario=? WHERE id=?', [mensaje.texto_mensaje, mensaje.fecha_mensaje, mensaje.id_usuario, id], function (err,res){
+    if (err) {
+      console.log('error: ', err);
+      result(null, err);
+    } else {
+      result(null, res);
+    }
+  });
+};
+
+Mensaje.delete = function(id, result){
+  dbConn.query('DELETE FROM mensaje WHERE id = ?', [id], function (err, res) {
+    if (err) {
+      console.log('error: ', err);
+      result(null, err);
+    }
+    else {
+      result(null, res);
+    };
+  });
+};
+
 module.exports = Mensaje;
